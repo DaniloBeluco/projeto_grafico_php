91b69b4cb1b62d66af25c9437ef656f748e6a1dd
danil <danil@LAPTOP-64HP4IHL>
2020-01-20 11:49:50 -0300

    Esse é meu segundo commit

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+$vendas = array(50, 20, 30, 40, 20);
+$custos = array(28, 14, 74, 23, 98);
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+
+        <div style="width:500px;">
+            <canvas id="grafico"></canvas>  <!--tag html usada em graficos-->
+
+        </div>
+        <script type="text/javascript" src="Chart.min.js"></script>      <!--carrega a biblioteca chart-->
+        <script type="text/javascript">
+            window.onload = function () {
+                var contexto = document.getElementById("grafico").getContext("2d");
+                var grafico = new Chart(contexto, {//cria um novo grafico
+                    type: 'line', //tipo do grafico
+                    data: {
+                        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril'], //labels do grafico
+                        datasets: [{
+                                label: 'Vendas',
+                                backgroundColor: 'red', //cor da linha do grafico
+                                borderColor: 'red',
+                                data: [<?php echo implode(',', $vendas); ?>], //valor de cada label
+                                fill: false    //pintura de dentro
+                            }, {
+                                label: 'Custos',
+                                backgroundColor: 'blue',
+                                borderColor: 'blue',
+                                data: [<?php echo implode(',', $custos); ?>],
+                                fill: false
+                            }
+                        ],
+                    }
+                });
+            }
+
+
+
+        </script>
     </body>
 </html>
